9a320493ea19bb7b9e354ce24e9a9045b25a8982
System Administrator <root@YDs-MacBook-Air.local>
2016-10-20 00:24:53 -0700

    add async solution

--- app/async.js
+++ app/async.js
@@ -2,10 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+  	return new Promise(function(resolve, reject) {
+  		resolve(value);
+  	})
 
   },
 
   manipulateRemoteData: function(url) {
+  	return new Promise(function(resolve, reject) {
+  		var xhr = new XMLHttpRequest();
+  		xhr.onreadystatechange = function() {
+  			if (xhr.readyState === 4 && xhr.status === 200) {
+  				var data = JSON.parse(xhr.responseText);
+  				var names = data.people.map(function(elem) {
+  					return elem.name;
+  				}).sort();
+  				resolve(names);
+  			}
+  		}
+  		xhr.open('GET', url, true);
+  		xhr.send();
+  	})
 
   }
 };
