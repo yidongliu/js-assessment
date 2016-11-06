7e01c2451c737de8d086593caea48ca1ee97d7ad
YD Liu <YidongLiu@YDs-MacBook-Air.local>
2016-11-05 22:52:41 -0700

    add part of solution

--- app/recursion.js
+++ app/recursion.js
@@ -2,6 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var list = [];
+    iterate(data, list);
+    return list;
+
+    function iterate(data, res) {
+      for (var key in data) {
+        if (typeof data[key] !== 'object') {
+          if (data[key].indexOf('.') > -1) {
+            res.push(data[key]);
+          }
+        } else {
+          iterate(data[key], res);
+        }
+      }
+    }
 
   },
 
@@ -10,7 +25,14 @@ exports.recursionAnswers = {
   },
 
   fibonacci: function(n) {
+    if (n <= 1) {
+      return n;
+    }
 
+    if (n === 2) {
+      return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
   },
 
   validParentheses: function(n) {
--- app/regex.js
+++ app/regex.js
@@ -2,6 +2,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
+    
 
   },
 
